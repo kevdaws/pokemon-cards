71a32dfe9466b5a064748e30aac8c63981270f4d
kevdaws <62354600+kevdaws@users.noreply.github.com>
2021-02-24 17:02:05 -0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,27 @@
+const cards = document.querySelector('#cards');
+
+var button = document.getElementById('submit');
+button.addEventListener('click', createAPIURL);
+
+var input = document.getElementById('pokemon');
+
+function createAPIURL() {
+  api_path = "https://api.pokemontcg.io/v1/cards?name="
+  api_url = api_path + input.value
+  console.log(input.value);
+  getPokemonCards(api_url);
+}
+
+async function getPokemonCards(api_url) {
+
+  const response = await fetch(api_url);
+  console.log(response);
+  const json = await response.json();
+  
+  json.cards.forEach((card) => {
+    const image = document.createElement('img');
+    image.src = card.imageUrl;
+    cards.append(image);
+  });
+}
+
